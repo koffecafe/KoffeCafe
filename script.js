b081b3e1c77f717598b5ecbd4f5cf3e211a8b567
koffecafe <koffecafe.online@gmail.com>
2024-10-12 01:09:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open')
+}
+
 let slideIndex = 0;
         const slides = document.querySelectorAll('.slide');
 
